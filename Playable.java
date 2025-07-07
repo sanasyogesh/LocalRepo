a7af0b23e3c5a4e2e872f2cd97a324a8664b0406
sanasyogesh@gmail.com <sanasyogesh@gmail.com>
2025-07-07 18:29:20 +0530

    Error Removed.

--- Playable.java
+++ Playable.java
@@ -1,6 +1,6 @@
 public interface Playable {
     public void pay();
     public void receive();
-    public void generateTransactionId(String productId);
+    public void generateTransactionId(int productId);
     public void generateTransactionId(String productName);
 }
